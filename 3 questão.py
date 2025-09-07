6936a635e660769c29f2b576ed603bac6dc4b647
vitor19931 <vitor.a.freitas@ba.estudante.senai.br>
2025-09-07 18:17:34 -0300

    Update 3 questão.py

---
+++ 3 questão.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls || clear")
+
+a = int(input("Informe o valor de a: "))
+b = int(input("Informe o valor de b: "))
+
+if a == b:
+    c = a + b
+else:
+    c = a * b 
+
+print("Resultado", c)
