824bf4b06f85f212a13e0a3a0b3e4a1de29956dd
vitor19931 <vitor.a.freitas@ba.estudante.senai.br>
2025-09-07 18:17:59 -0300

    Update 6 questão.py

---
+++ 6 questão.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls || clear")
+
+nota1 = float(input("Digite a nota1:"))
+nota2 = float(input("Digite a nota2:"))
+
+
+media = (nota1 + nota2) /2
+
+
+if media >=6:
+    print("Parabens você passou")
+elif media >= 4.1 and media <= 5.9:
+    print("O aluno foi para recuperação")
+else:
+    print("o aluno foi reprovado")  
